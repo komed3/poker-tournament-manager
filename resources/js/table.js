12e1819faf711009a2da36f165cdd793c3209305
Paul Köhler <41899980+komed3@users.noreply.github.com>
2021-03-01 21:03:39 +0100

    fixing

--- resources/js/table.js
+++ resources/js/table.js
@@ -2,8 +2,6 @@ jQuery( document ).ready( function( $ ) {
     
     Highcharts.setOptions( highcharts_options );
     
-    console.log(  );
-    
     var ptm_chart_stacks = Highcharts.stockChart( 'ptm_chart_stacks', {
         
         plotOptions: {
